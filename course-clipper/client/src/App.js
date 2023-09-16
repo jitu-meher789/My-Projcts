d74f80b76c4a6c1ab4923c5d43f54839c436efcf
“beast22803” <varshit.madi@gmail.com>
2023-09-16 11:41:23 +0530

    Completed Course-Clipper

---
+++ course-clipper/client/src/App.js
@@ -0,0 +1,58 @@
+import Home from "./Pages/Components/Home/Home";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Contact from "./Pages/Components/Contact/Contact";
+import Blog from "./Pages/Components/Blog/Blog";
+import Review from "./Pages/Components/Reviews/Reviews";
+import Comparison from "./Pages/Components/Comparison/Comparison";
+import Privacy from "./Pages/Components/Privacy/Privacy";
+import Term from "./Pages/Components/Term/Term";
+import About from "./Pages/Components/About/About";
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+    {
+      path: "/blog",
+      element: <Blog />,
+    },
+    {
+      path: "/review",
+      element: <Review />,
+    },
+    {
+      path: "/comparison",
+      element: <Comparison />,
+    },
+    {
+      path: "/privacy",
+      element: <Privacy />,
+    },
+    {
+      path: "/term",
+      element: <Term />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ course-clipper/client/src/Pages/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../App.css";
+
+function Footer() {
+    return (
+        <div className="Main">
+            <div className="footer-main">
+                <div>
+                    <p className="footer-p">
+                        Copyright &#169; 2023 SGAMPL-All rights reserved.
+                    </p>
+                </div>
+                <div>
+                    <p className="footer-p">
+                        Powered by Kronoryx Technology Pvt. Ltd.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
